6774c8437b98e38ea8d7d478b60624aa14b40851
TungYing <polppk@gmail.com>
2016-08-11 00:18:54 +0800

    修正 Pseudocode

--- A4_SearchWord.cs
+++ A4_SearchWord.cs
@@ -1,7 +1,13 @@
 /**
 	暴力法
-	1.for each 每一個node對他做搜尋
-	
-	2. 每一個nodec會經由上下左右四個方向去找尋是否有此字串，使用遞迴
+	foreach subBoard in board :  
+	 foreach node in subBoard :
+		if node的第一個字元是否跟word的第一個字元一樣: 
+			if( findNext(node) == true)
+				return ture;
+	return false;
+			
+	function findNext(board,currentNode, row, col,word): 
+		每一個node會經由上下左右四個方向去找尋是否有此字串，使用遞迴
 
 **/
